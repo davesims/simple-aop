6464343c82b756095bdde013bff1a952e01d0324
Dave Sims <davsims@gmail.com>
2011-04-28 11:37:36 -0500

    rdoc edits for multi-around

--- lib/linker.rb
+++ lib/linker.rb
@@ -1,9 +1,10 @@
 
+# allow Symbols to link with 'next' interface
 class Symbol
   attr_accessor :next
 end
 
-# add an interface to Array to allow adding and linking objects incrementally
+# add an interface to Array to allow adding and linking symbols incrementally
 class Array
   def add_and_link(value)
     Array(value).each do |item|
